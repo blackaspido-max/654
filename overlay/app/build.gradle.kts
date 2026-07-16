6cae8be5b6011994788681933ae68ff8c1083a89
blackaspido-max <blackaspido@gmail.com>
2026-07-16 10:28:13 +0500

    Add Android app module configuration

---
+++ overlay/app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "ru.aspid.nightmaster"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "ru.aspid.nightmaster"
+        minSdk = 33
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1-prototype"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.androidx)
+    implementation(libs.material)
+    implementation(project(":lib"))
+}
