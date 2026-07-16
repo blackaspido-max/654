2d1e8e51290f642863ae6f475551db8953d4cffd
blackaspido-max <blackaspido@gmail.com>
2026-07-16 22:29:42 +0500

    Add application dependency container

---
+++ overlay/app/src/main/java/ru/aspid/nightmaster/NightMasterApplication.kt
@@ -0,0 +1,34 @@
+package ru.aspid.nightmaster
+
+import android.app.Application
+import androidx.room.Room
+import ru.aspid.nightmaster.core.inference.InferenceController
+import ru.aspid.nightmaster.core.inference.LlamaInferenceController
+import ru.aspid.nightmaster.data.database.NightMasterDatabase
+import ru.aspid.nightmaster.data.preferences.SettingsRepository
+
+class NightMasterApplication : Application() {
+    val database: NightMasterDatabase by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            NightMasterDatabase::class.java,
+            "night-master.db",
+        ).build()
+    }
+
+    val settingsRepository: SettingsRepository by lazy {
+        SettingsRepository(applicationContext)
+    }
+
+    val inferenceController: InferenceController by lazy {
+        LlamaInferenceController(applicationContext)
+    }
+
+    override fun onTerminate() {
+        if (this::class.java.desiredAssertionStatus()) {
+            inferenceController.close()
+            database.close()
+        }
+        super.onTerminate()
+    }
+}
