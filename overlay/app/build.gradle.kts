16ba87d7f13f33eb4bbb423e0ef059e24643c60e
blackaspido-max <blackaspido@gmail.com>
2026-07-16 11:19:27 +0500

    Bump prototype version to 0.2

--- overlay/app/build.gradle.kts
+++ overlay/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ru.aspid.nightmaster"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1-prototype"
+        versionCode = 2
+        versionName = "0.2-prototype"
 
         vectorDrawables {
             useSupportLibrary = true
