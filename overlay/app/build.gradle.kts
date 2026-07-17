485adfe834d8a989a3043df8799f7a4dd72441c1
blackaspido-max <blackaspido@gmail.com>
2026-07-17 07:23:14 +0500

    Version Android file descriptor hotfix as 0.7.1

--- overlay/app/build.gradle.kts
+++ overlay/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "ru.aspid.nightmaster"
         minSdk = 33
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.7-model-manager"
+        versionCode = 8
+        versionName = "0.7.1-model-manager-fd-fix"
 
         vectorDrawables {
             useSupportLibrary = true
